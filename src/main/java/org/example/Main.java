1b5eb90802a2682c916159ae97b22ba8d926eae0
Alisa Semenova <salisa4227@gmail.com>
2022-10-28 10:30:59 +0300

    Буратина и яблоки.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       int buratinaApple = 12;
+       int karabasTakeawayapple = 8;
+       int appleAftertakeaway = buratinaApple - karabasTakeawayapple;
+       System.out.println(appleAftertakeaway);
+    }
+}
